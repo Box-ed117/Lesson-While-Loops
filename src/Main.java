966f05427ccaf4168d47d225740108a94e269694
Box-ed117 <hayden.cheng27@ycdsbk12.ca>
2025-11-04 17:39:21 -0500

    Completed Practice || #1 Count up

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,15 @@
 public class Main extends ConsoleProgram {
     @Override
     public void run() {
-        System.out.println("Hello, World!");
+        //--------------------| Practice Problems |--------------------
+
+
+        //---------------| #1 Count up |---------------
+        int numbers = 1; // Starting Number
+        while (numbers <= 10) { // Repeats untill...
+            System.out.println(numbers);
+            numbers += 1; // Increases by...
+        }
+
     }
 }
